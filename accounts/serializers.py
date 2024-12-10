89de801b6f02c28c4f3a69de17bd54e94712814c
mufiii <mufeesrahman9795@gmail.com>
2024-12-10 12:56:28 +0530

    profile updated

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -19,3 +19,14 @@ class UserLoginSerializer(serializers.Serializer):
     email = serializers.CharField()
     password = serializers.CharField(write_only=True)
     
+    
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'name', 'phone', 'gender']
+        
+        
+class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id','name', 'phone', 'gender']
