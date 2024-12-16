9f3b1f9f642433aad7673ece54ad0325383f5841
ahdsalman <ahammedsalmanp3@gmail.com>
2024-12-16 14:27:43 +0530

    Resolved conflicts in the code

--- package/models.py
+++ package/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-
-from django.db import models
-
 class Package(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=500)   
