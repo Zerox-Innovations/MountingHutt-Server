3ea504125160e6f76a529522c921692ef643a02d
ahdsalman <ahammedsalmanp3@gmail.com>
2024-12-18 20:13:01 +0530

    Remove .pyc files and update .gitignore

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from accounts.models import CustomUser
-
-
-@admin.register(CustomUser)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from accounts.views import UserRegistrationAPIView,UserLoginAPIView,UserProfileAPIView
-
-urlpatterns = [
-    path('register/',UserRegistrationAPIView.as_view(),name='register'),
-    path('login/',UserLoginAPIView.as_view(),name='login'),
-    path('profile/',UserProfileAPIView.as_view(),name='login')
-]
